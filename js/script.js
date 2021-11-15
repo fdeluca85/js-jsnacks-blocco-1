b7cb70615390d116ed790762433de2cafe637ad8
francesca <fradeluca85@gmail.com>
2021-11-15 15:41:01 +0100

    jsnack 2

--- js/script.js
+++ js/script.js
@@ -26,3 +26,26 @@
 //     console.log(numeri);
 // }
 // console.log('la somma è '+ somma);
+
+
+// JSnack 2
+// Dare la possibilità di inserire due parole.
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+const  parola1 = prompt("inserisci una parola");
+const  parola2 = prompt("inserisci una parola");
+console.log(parola1);
+console.log(parola2);
+
+if   (parola1.length > parola2.length){
+console.log("la parola piú lunga è " + parola1); 
+}
+else if(parola1.length < parola2.length ){
+    console.log("la parola piú lunga è " + parola2);
+}
+else{
+     console.log(parola1 + " e " + parola2);
+}
+
+
